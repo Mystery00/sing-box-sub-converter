268db607532c7b99798b81ef5aa32924139c25fe
Mystery0 <mystery0dyl520@gmail.com>
2025-07-06 18:26:22 +0800

    为配置结构新增 mapstructure 标签以支持配置解析

--- internal/config/types.go
+++ internal/config/types.go
@@ -1,16 +1,16 @@
 package config
 
 type Subscription struct {
-	URL       string `json:"url"`
-	Tag       string `json:"tag"`
-	Prefix    string `json:"prefix"`
-	UserAgent string `json:"userAgent"`
+	URL       string `json:"url" mapstructure:"url"`
+	Tag       string `json:"tag" mapstructure:"tag"`
+	Prefix    string `json:"prefix" mapstructure:"prefix"`
+	UserAgent string `json:"userAgent" mapstructure:"userAgent"`
 }
 
 type ProvidersGlobalConfig struct {
-	Subscribes      []Subscription `json:"subscribes"`
-	Prefix          bool           `json:"prefix"`
-	Emoji           bool           `json:"emoji"`
-	ExcludeProtocol string         `json:"exclude_protocol"`
-	ShowSubInNodes  bool           `json:"show_sub_in_nodes"`
+	Subscribes      []Subscription `json:"subscribes" mapstructure:"subscribes"`
+	Prefix          bool           `json:"prefix" mapstructure:"prefix"`
+	Emoji           bool           `json:"emoji" mapstructure:"emoji"`
+	ExcludeProtocol string         `json:"exclude_protocol" mapstructure:"exclude_protocol"`
+	ShowSubInNodes  bool           `json:"show_sub_in_nodes" mapstructure:"show_sub_in_nodes"`
 }
